27f32a1bd05909e06aec3e41ef464506e15e7d86
krishnaiitd <kprasad.iitd@gmail.com>
2014-08-18 08:49:42 +0530

    Added basic layout from layout it

--- analyzetweets/main.py
+++ analyzetweets/main.py
@@ -1,38 +1,39 @@
-# Main application file: "webapp/main.py"
 import os
-    
+
 from google.appengine.api import memcache, users
 from google.appengine.ext import db, webapp
 from google.appengine.ext.webapp.template import render
 from google.appengine.ext.webapp.util import run_wsgi_app
 
-class Greeting(db.Model):
-    author = db.UserProperty()
-    content = db.TextProperty()
-    date = db.DateTimeProperty(auto_now_add=True)
+class TweetsText(db.Model):
+    createdAt   = db.DateTimeProperty(auto_now_add=True)
+    author      = db.UserProperty()
+    companyName = db.TextProperty()
+    tweetText   = db.TextProperty()
 
 class MainHandler(webapp.RequestHandler):
     def get(self):
         user = users.get_current_user()
-        greetings = memcache.get('greetings')
-        if not greetings:
-            greetings = Greeting.all().order('-date').fetch(10)
-            memcache.add('greetings', greetings)
+        tweetstext = memcache.get('tweetstext')
+        if not tweetstext:
+            tweetstext = TweetsText.all().order('-createdAt').fetch(10)
+            memcache.add('tweetstext', tweetstext)
         context = {
             'user':      user,
-            'greetings': greetings,
+            'tweetstext': tweetstext,
             'login':     users.create_login_url(self.request.uri),
             'logout':    users.create_logout_url(self.request.uri),
         }
-        tmpl = os.path.join(os.path.dirname(__file__), 'index.html')
+        tmpl = os.path.join(os.path.dirname(__file__), 'top10tweets.html')
         self.response.out.write(render(tmpl, context))
 
 class GuestBook(webapp.RequestHandler):
     def post(self):
-        greeting = Greeting()
-        greeting.content = self.request.get('content')
-        greeting.put()
-        memcache.delete('greetings')
+        tweetsText = TweetsText()
+
+        tweetsText.tweetText = self.request.get('content')
+        tweetsText.put()
+        memcache.delete('tweetstext')
         self.redirect('/')
 
 application = webapp.WSGIApplication([
